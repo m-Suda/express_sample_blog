f72a07b866d50f6f1b0e86fc47e4fc9a69d8c5b8
masak <oicwam_m-Suda@users.noreply.github.com>
2018-11-26 01:13:58 +0900

    express_sample_blog 新規作成、更新、削除、エラー処理

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
-var post = require('./routes/post');
 // Express4.xでput, deleteを使用するとき必要
+var connect = require('connect');
 var methodOverride = require('method-override');
+// Express4.x系ではCSRF対策に必要なミドルウェアは別途インストールする必要がある
+var cookieParser = require('cookie-parser');
+var expressSession = require('express-session');
+var csrf = require('csurf');
+// 自作モジュールの読み込み
+var post = require('./routes/post');
+
+
 
 app = express();
 
@@ -14,20 +22,40 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 // Express4.xでput, deleteを使用するときは別モジュールを使用する。
-app.use(methodOverride('_method'));
+// これだとformのactionの中に?_method=PUTと入れる必要がある。
+// app.use(methodOverride('_method'));
+// 下記の処理を加えることでhiddenのname='_method'のvalueにPUTかDELETEを指定できるようになる。
+app.use(methodOverride((req, res) => {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        var method = req.body._method;
+        delete req.body._method;
+        return method;
+    }
+}));
+// csrf対策 ※json, urlencodedより後、morganよりも先に書くようにする。
+app.use(cookieParser());
+app.use(expressSession({secret: 'secret_key'}));
+app.use(csrf());
+app.use((req, res, next) => {
+    res.locals.csrftoken = req.csrfToken();
+    next();
+})
 
 app.use(logger('dev'));
 
 // routing
 app.get('/', post.index);
-app.get('/posts/:id', post.show);
-/*
+app.get('/posts/:id([0-9]+)', post.show);
 app.get('/posts/new', post.new);
 app.post('/posts/create', post.create);
-app.get('/posts:id/edit', post.edit);
-app.put('/posts:id/', post.update);
-app.delete('/posts:id/', post.destroy);
-*/
+app.get('/posts/:id/edit', post.edit);
+app.put('/posts/:id', post.update);
+app.delete('/posts/:id/', post.destroy);
+
+// エラー処理 ※Express4.xではapp.routerが無くトレースがそのまま表示されてしまうためルーティングの下に記述する。
+app.use((error, req, res, next) => {
+    res.send(error.message);
+});
 
 app.listen(3000);
 console.log('Server starting...');
--- routes/post.js
+++ routes/post.js
@@ -15,3 +15,62 @@ exports.show = (req, res) => {
 
     res.render('posts/show', {post: posts[req.params.id]});
 };
+
+
+// 新規作成画面
+exports.new = (req, res) => {
+
+    res.render('posts/new');
+};
+
+// 新規作成処理
+exports.create = (req, res) => {
+
+    var post = {
+        title: req.body.title,
+        body: req.body.body
+    };
+
+    posts.push(post);
+    res.redirect('/');
+};
+
+// 編集画面
+exports.edit = (req, res) => {
+
+    res.render('posts/edit',
+        {
+            post: posts[req.params.id],
+            id: req.params.id
+        }
+    );
+};
+
+// 更新処理
+exports.update = (req, res, next) => {
+
+    if (req.body.id !== req.params.id) {
+        next(new Error('ID is not valid'));
+        return;
+    }
+
+    posts[req.body.id] = {
+        title: req.body.title,
+        body: req.body.body
+    };
+    res.redirect('/');
+
+};
+
+// 削除処理
+exports.destroy = (req, res, next) => {
+
+    if (req.body.id !== req.params.id) {
+        next(new Error('ID is not valid'));
+        return;
+    }
+
+    // 指定された配列の要素を指定された数の分削除する。
+    posts.splice(req.body.id, 1);
+    res.redirect('/');
+}
